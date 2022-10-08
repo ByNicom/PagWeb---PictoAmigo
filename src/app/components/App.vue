04cc8d9265ec526e3c4ba5aec6ce5e78e6756a2e
Nicom <nicolas.mandujano@usm.cl>
2022-10-08 20:26:26 -0300

    prueba 1

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,11 +1,26 @@
-<template>
-   <nav class="navbar nabar-light bg-light">
-    <a href="/" class="navbar-brand">Menv Stack</a>
-   </nav>
+
+<template>  
+   <div id="app" class="app">
+      <div class="img-container">
+            <img src="\src\assets\images\main.png" alt="">
+        </div>
+        <div class="form" @submit.prevent="HandleSubmit">
+            <p class="label">Iniciar sesion</p>
+            <div class="input-container">
+                <custom-input
+                v-for="(input, i) in inputs"
+                :key="i"
+                v-model="input.value"
+                :type="input.type"
+                :label="input.label"/>
+            </div>
+            <p class="space"><a href="#">¿Olvidaste tu contraseña?</a></p>
+            <button class="login-button">Ingresar</button>
+            <p><a href="#">Registrate aca!!</a></p>
+        </div>
+    </div>
 </template>
 
 <script>
-</script>
 
-<style>
-</style>
+    </script>
