20d8bc7eb2b6d8f21a9df021e73b9add11a6c4d0
Nicom <nicolas.mandujano@usm.cl>
2022-10-08 00:16:41 -0300

    proyecto inicial

---
+++ src/app/components/App.vue
@@ -0,0 +1,11 @@
+<template>
+   <nav class="navbar nabar-light bg-light">
+    <a href="/" class="navbar-brand">Menv Stack</a>
+   </nav>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/routes/tasks.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/',(req, res)=>{
+    res.send('API Tasks is goes here')
+});
+
+module.exports = router;
